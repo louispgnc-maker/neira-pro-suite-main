4138d6f3d5215b20a09728eddcdede845bfb8d1e
louispgnc-maker <louispgnc@gmail.com>
2025-11-20 17:00:14 +0100

    Clean up main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,4 +5,3 @@ import "./index.css";
 // FullCalendar styles are loaded via CDN (v5) because the v6 npm packages don't include CSS files
 
 createRoot(document.getElementById("root")!).render(<App />);
-Cache bust
