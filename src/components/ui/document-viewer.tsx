44fdbcedab0dbcf24278b9d6078a96e6441d0c94
louispgnc-maker <louispgnc@gmail.com>
2025-11-22 19:25:43 +0100

    Fix: corriger l'URL du worker PDF.js (https au lieu de //)

--- src/components/ui/document-viewer.tsx
+++ src/components/ui/document-viewer.tsx
@@ -7,7 +7,7 @@ import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
 // Configure PDF.js worker
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 interface DocumentViewerProps {
   open: boolean;
