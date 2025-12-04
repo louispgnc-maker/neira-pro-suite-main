0bd72ad8fa4474a27350794257b4c2189e8b90d9
louispgnc-maker <louispgnc@gmail.com>
2025-12-04 20:11:40 +0100

    feat: save ALL form fields to client record
    - Map all 50+ form fields to corresponding client columns
    - Save identity info: sexe, type_identite, numero_identite, date_expiration
    - Save professional info: employeur, adresse_professionnelle, siret
    - Save financial info: revenus, situation_fiscale, comptes_bancaires
    - Save case info: type_dossier, contrat_souhaite, historique_litiges
    - Save family info: situation_matrimoniale, enfants (jsonb), situation_familiale (jsonb)
    - Save consent: consentement_rgpd, signature_mandat
    - All form data now visible in client record

--- supabase/migrations/20251204_fix_submit_client_form_function.sql
+++ supabase/migrations/20251204_fix_submit_client_form_function.sql
@@ -28,6 +28,7 @@ BEGIN
   v_creator_role := COALESCE(v_form_record.creator_role, 'notaire');
 
   -- Create client record in clients table with owner_id from form creator
+  -- Map all form fields to corresponding client columns
   INSERT INTO clients (
     owner_id,
     role,
@@ -40,7 +41,26 @@ BEGIN
     date_naissance,
     lieu_naissance,
     nationalite,
+    sexe,
+    type_identite,
+    numero_identite,
+    date_expiration_identite,
     profession,
+    employeur,
+    adresse_professionnelle,
+    siret,
+    situation_fiscale,
+    type_dossier,
+    contrat_souhaite,
+    historique_litiges,
+    consentement_rgpd,
+    signature_mandat,
+    etat_civil,
+    situation_matrimoniale,
+    enfants,
+    revenus,
+    comptes_bancaires,
+    situation_familiale,
     source
   )
   VALUES (
@@ -55,7 +75,35 @@ BEGIN
     NULLIF(form_response->>'date_naissance', '')::DATE,
     form_response->>'lieu_naissance',
     form_response->>'nationalite',
+    form_response->>'sexe',
+    form_response->>'type_piece_identite',
+    form_response->>'numero_piece_identite',
+    NULLIF(form_response->>'date_expiration_piece', '')::DATE,
     form_response->>'profession',
+    form_response->>'nom_employeur',
+    form_response->>'adresse_professionnelle',
+    form_response->>'numero_siret',
+    form_response->>'situation_fiscale',
+    form_response->>'nature_affaire',
+    form_response->>'type_contrat',
+    form_response->>'litiges_passes',
+    COALESCE((form_response->>'consentement_rgpd')::BOOLEAN, false),
+    COALESCE((form_response->>'signature_mandat')::BOOLEAN, false),
+    form_response->>'etat_civil',
+    form_response->>'situation_matrimoniale',
+    CASE WHEN form_response->>'enfants_details' IS NOT NULL 
+         THEN form_response->'enfants_details' 
+         ELSE '[]'::jsonb END,
+    form_response->>'revenus_mensuels',
+    CASE WHEN form_response->>'comptes_bancaires' IS NOT NULL 
+         THEN form_response->'comptes_bancaires' 
+         ELSE '[]'::jsonb END,
+    jsonb_build_object(
+      'situation_familiale', form_response->>'situation_familiale',
+      'regime_matrimonial', form_response->>'regime_matrimonial',
+      'nombre_enfants', form_response->>'nombre_enfants',
+      'personne_a_charge', form_response->>'personne_a_charge'
+    ),
     'formulaire_web'
   )
   RETURNING id INTO v_client_id;
