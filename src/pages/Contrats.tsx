662a95bbcf10fa80741e684aad83ec5c629e7a4a
louispgnc-maker <louispgnc@gmail.com>
2025-12-17 19:29:39 +0100

    Fix: Ajout import Checkbox manquant

--- src/pages/Contrats.tsx
+++ src/pages/Contrats.tsx
@@ -44,6 +44,7 @@ import {
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Checkbox } from "@/components/ui/checkbox";
 import { toast } from "sonner";
 import { ShareToCollaborativeDialog } from "@/components/cabinet/ShareToCollaborativeDialog";
 
